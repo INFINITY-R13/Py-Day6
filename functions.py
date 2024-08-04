bac1b774fbce4b6e07bce1d9967b8cbb13638cd3
Raghuraj Singh <hannuseven@gmail.com>
2024-08-04 12:05:43 +0530

    learning functions

---
+++ functions.py
@@ -0,0 +1,15 @@
+# print is a function
+print("Hello")
+
+#length function
+my_length = len("hello")
+print(my_length)
+
+#defining a function
+def my_function():
+    print("it is a function")
+
+#calling a function
+my_function()   
+
+
